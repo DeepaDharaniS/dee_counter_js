be736aea443907d87c09d9dd6a2beee880524799
Deepa Dharani Sakthivel <144089170+DeepaDharaniS@users.noreply.github.com>
2023-11-28 10:11:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+// let count = 0;
+
+// document.getElementById("decBtn").onclick = function () {
+//     count -= 1;
+//     document.getElementById("res").innerHTML = count;
+// }
+
+
+// document.getElementById("resBtn").onclick = function () {
+//     count = 0;
+//     document.getElementById("res").innerHTML = count;
+// }
+
+
+// document.getElementById("incBtn").onclick = function () {
+//     count += 1;
+//     document.getElementById("res").innerHTML = count;
+// }
+
+
+
+
+let i = 0;
+
+document.getElementById("decBtn").onclick = function () {
+    i--;
+    document.getElementById("res").innerHTML = i;
+}
+
+
+document.getElementById("resBtn").onclick = function () {
+    i = 0;
+    document.getElementById("res").innerHTML = i;
+}
+
+
+document.getElementById("incBtn").onclick = function () {
+    i++;
+    document.getElementById("res").innerHTML = i;
+}
